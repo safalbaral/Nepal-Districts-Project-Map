5b5982388a921879bc42bb4fc66eceb57d114847
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-08-10 10:48:19 +0545

    Made website more responsive

--- src/components/BigStats.tsx
+++ src/components/BigStats.tsx
@@ -7,32 +7,36 @@ const BigStats = ({ selectedMarker, totalProjectsInSelected }) => {
 
   useEffect(() => {
     loadProjectsData(setProjectsData);
-    const calculateUniqueDistricts = () => {
-      // Use a Set to keep track of unique districts
-      const uniqueDistricts = new Set();
+  }, []);
 
-      projectsData.forEach((project) => {
-        // Add each district in the current project's district array to the Set
-        project.district.forEach((district) => {
-          uniqueDistricts.add(district);
-        });
-      });
-      setTotalDistricts(uniqueDistricts.size);
-    };
-
+  useEffect(() => {
     if (projectsData) {
+      const calculateUniqueDistricts = () => {
+        // Use a Set to keep track of unique districts
+        const uniqueDistricts = new Set();
+
+        projectsData.forEach((project) => {
+          // Add each district in the current project's district array to the Set
+          project.district.forEach((district) => {
+            uniqueDistricts.add(district);
+          });
+        });
+        setTotalDistricts(uniqueDistricts.size);
+      };
       calculateUniqueDistricts();
     }
-  }, []);
+  }, [projectsData]);
 
   if (selectedMarker) {
     return (
       <div>
         <div className="flex flex-col items-center justify-center p-6 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-lg shadow-lg">
-          <div className="text-3xl font-bold mb-2">
+          <div className="text-3xl font-bold mb-2 max-sm:text-lg">
             {totalProjectsInSelected} Projects Completed
           </div>
-          <div className="text-xl">In {selectedMarker.district} District</div>
+          <div className="text-xl max-sm:text-base">
+            In {selectedMarker.district} District
+          </div>
         </div>
       </div>
     );
@@ -45,10 +49,12 @@ const BigStats = ({ selectedMarker, totalProjectsInSelected }) => {
   ) : (
     <div>
       <div className="flex flex-col items-center justify-center p-6 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-lg shadow-lg">
-        <div className="text-3xl font-bold mb-2">
+        <div className="text-3xl font-bold mb-2 max-sm:text-lg">
           {projectsData.length} Projects Completed
         </div>
-        <div className="text-xl">Across {totalDistricts} Districts</div>
+        <div className="text-xl max-sm:text-base">
+          Across {totalDistricts} Districts of Nepal
+        </div>
       </div>
     </div>
   );
