5b1f6a8c7fe5dbf687f660c3f001b949af4dc703
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-08-10 18:45:08 +0545

    Committed file

---
+++ src/utils/calcUtils.ts
@@ -0,0 +1,19 @@
+import { loadProjectsData } from "./loaderUtils";
+
+export const calculateProjectsInDistrict = async (district) => {
+  try {
+    const response = await fetch("/data/projects-data.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const filteredProjects = data.filter((project) =>
+      project.district.includes(district)
+    );
+
+    // Return the number of filtered projects
+    return filteredProjects.length;
+  } catch (e) {
+    console.error("Error loading projects data:", e);
+  }
+};
